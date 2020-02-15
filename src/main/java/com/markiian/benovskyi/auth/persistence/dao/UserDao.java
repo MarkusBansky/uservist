f5d9438bac31a9fad5334247b1ff35dc6f526209
MarkusBansky <MarkusBansky@users.noreply.github.com>
2020-02-15 17:07:01 +0000

    Added settings and properties

---
+++ src/main/java/com/markiian/benovskyi/auth/persistence/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.markiian.benovskyi.auth.persistence.dao;
+
+import com.markiian.benovskyi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+    @Query
+    Optional<User> findById();
+}
