a608e7ee78d247c8d4b974aae4b0abc5c26f7eed
Markiian benovskyi <admin@markiian-benovskyi.com>
2020-07-27 10:34:24 +0100

    Fixed users controller

--- uservist-service/src/main/java/com/markiian/benovskyi/auth/controller/UsersController.java
+++ uservist-service/src/main/java/com/markiian/benovskyi/auth/controller/UsersController.java
@@ -1,12 +1,8 @@
 package com.markiian.benovskyi.auth.controller;
 
-import com.markiian.benovskyi.auth.security.CurrentUser;
 import com.markiian.benovskyi.auth.service.UserService;
-import com.markiian.benovskyi.auth.util.ApplicationConstants;
 import com.markiian.benovskyi.uservist.api.uservist_api.api.UsersApi;
-import com.markiian.benovskyi.uservist.api.uservist_api.model.InlineObject;
-import com.markiian.benovskyi.uservist.api.uservist_api.model.UserDto;
-import it.ozimov.springboot.mail.service.exception.CannotSendEmailException;
+import com.markiian.benovskyi.uservist.api.uservist_api.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,37 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UsersController implements UsersApi {
 
-    private final UserPermissionService userPermissionService;
     private final UserService userService;
 
     @Autowired
-    public UsersController(UserService userService, UserPermissionService userPermissionService) {
+    public UsersController(UserService userService) {
         this.userService = userService;
-        this.userPermissionService = userPermissionService;
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity usersGetAll(@NotNull @Valid String service, @NotNull @Valid Integer page) {
-        if (userPermissionService.canGetAll(service, CurrentUser.getAuth())) {
-            return ResponseEntity.ok(userService.getAllUsers(page, service));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApplicationConstants.UNAUTHORIZED_EXCEPTION_MESSAGE);
+    public ResponseEntity<UsersPageDto> usersGetAll(@NotNull @Valid String service, @NotNull @Valid Integer page) {
+        return ResponseEntity.ok(userService.getAllUsers(page, service));
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity usersGetById(Long id) {
-        if (userPermissionService.canGetById(id, CurrentUser.getAuth())) {
-            return ResponseEntity.ok(userService.getUserById(id, CurrentUser.getServiceKey()));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApplicationConstants.UNAUTHORIZED_EXCEPTION_MESSAGE);
+    public ResponseEntity<UserDto> usersGetById(Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
     @Override
@@ -57,33 +44,20 @@ public class UsersController implements UsersApi {
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity usersCreate(@Valid UserDto userDto) {
-        if (userPermissionService.canCreate(userDto.getServiceRoles(), CurrentUser.getAuth())) {
-            try {
-                return ResponseEntity.ok(userService.createUserForService(userDto));
-            } catch (UnsupportedEncodingException | CannotSendEmailException e) {
-                e.printStackTrace();
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApplicationConstants.UNAUTHORIZED_EXCEPTION_MESSAGE);
+    public ResponseEntity<UserDto> usersCreate(@Valid UserCreateDto dto) {
+        return ResponseEntity.ok(userService.createUserForService(dto));
     }
 
     @Override
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity usersUpdate(@Valid UserDto userDto) {
-        if (userPermissionService.canUpdate(userDto, CurrentUser.getAuth())) {
-            return ResponseEntity.ok(userService.updateUser(userDto));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApplicationConstants.UNAUTHORIZED_EXCEPTION_MESSAGE);
+    public ResponseEntity<UserDto> usersUpdate(Long id, @Valid UserUpdateDto dto) {
+        return ResponseEntity.ok(userService.updateUser(id, dto));
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity usersDelete(Long id) {
-        if (userPermissionService.canDelete(id, CurrentUser.getAuth())) {
-            return ResponseEntity.ok(userService.deleteUser(id));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApplicationConstants.FORBIDDEN_EXCEPTION_MESSAGE);
+    public ResponseEntity<Void> usersDelete(Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
     }
 }
--- uservist-service/src/main/java/com/markiian/benovskyi/auth/service/UserService.java
+++ uservist-service/src/main/java/com/markiian/benovskyi/auth/service/UserService.java
@@ -2,28 +2,27 @@ package com.markiian.benovskyi.auth.service;
 
 import com.markiian.benovskyi.auth.persistance.dao.ServiceDao;
 import com.markiian.benovskyi.auth.persistance.dao.UserDao;
-import com.markiian.benovskyi.auth.persistance.mapper.ServiceRoleMapper;
 import com.markiian.benovskyi.auth.persistance.mapper.UserMapper;
 import com.markiian.benovskyi.auth.persistance.model.RoleType;
 import com.markiian.benovskyi.auth.persistance.model.Service;
 import com.markiian.benovskyi.auth.persistance.model.ServiceRole;
 import com.markiian.benovskyi.auth.persistance.model.User;
+import com.markiian.benovskyi.auth.security.CurrentUser;
 import com.markiian.benovskyi.auth.util.ApplicationConstants;
 import com.markiian.benovskyi.uservist.api.uservist_api.model.UserCreateDto;
 import com.markiian.benovskyi.uservist.api.uservist_api.model.UserDto;
 import com.markiian.benovskyi.uservist.api.uservist_api.model.UserUpdateDto;
-import it.ozimov.springboot.mail.service.exception.CannotSendEmailException;
+import com.markiian.benovskyi.uservist.api.uservist_api.model.UsersPageDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -33,7 +32,6 @@ public class UserService {
     private final ServiceDao serviceDao;
 
     private final UserMapper userMapper;
-    private final ServiceRoleMapper serviceRoleMapper;
 
     private final MailingService mailingService;
 
@@ -45,12 +43,10 @@ public class UserService {
     public UserService(UserDao userDao,
                        UserMapper userMapper,
                        ServiceDao serviceDao,
-                       ServiceRoleMapper serviceRoleMapper,
                        MailingService mailingService) {
         this.userDao = userDao;
         this.userMapper = userMapper;
         this.serviceDao = serviceDao;
-        this.serviceRoleMapper = serviceRoleMapper;
         this.mailingService = mailingService;
     }
 
@@ -73,26 +69,39 @@ public class UserService {
      * @param serviceKey The key for the service.
      * @return A page with users.
      */
-    public Page<UserDto> getAllUsers(Integer page, String serviceKey) {
+    public UsersPageDto getAllUsers(Integer page, String serviceKey) {
         LOGGER.info("Performing task to get all available users");
-        Pageable pageRequest = PageRequest.of(page, PAGE_SIZE);
-        Optional<com.markiian.benovskyi.auth.persistance.model.Service> service = serviceDao.findByKey(serviceKey);
 
-        if (service.isEmpty()) {
-            LOGGER.warn("Service for users request cannot be found, service key: {}", serviceKey);
-            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, ApplicationConstants.SERVICE_NOT_FOUND);
+        Pageable pageRequest = PageRequest.of(page, PAGE_SIZE, Sort.by("id"));
+        Page<User> users;
+
+        if (serviceKey == null || serviceKey.isBlank() && CurrentUser.isSuperUser()) {
+            users = userDao.findAll(pageRequest);
+        } else if (!serviceKey.isBlank()) {
+            Optional<Service> optionalService = serviceDao.findByKey(serviceKey);
+
+            if (optionalService.isEmpty()) {
+                LOGGER.warn("Service for users request cannot be found, service key: {}", serviceKey);
+                throw new HttpClientErrorException(HttpStatus.NOT_FOUND, ApplicationConstants.SERVICE_NOT_FOUND);
+            }
+
+            users = userDao.findAllUsersByServiceId(optionalService.get().getId(), pageRequest);
+        } else {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
         }
 
-        Page<User> users = userDao.findAllUsersByServiceId(service.get().getId(), pageRequest);
         LOGGER.info("Found {} users", users.getTotalElements());
 
-        Page<UserDto> resultPage = new PageImpl(
-                users.stream().map(userMapper::toDto).collect(Collectors.toList()),
-                pageRequest,
-                users.getTotalElements());
+        UsersPageDto dto = new UsersPageDto();
+        dto.setCurrentPage(page);
+        dto.setItemsPerPage(PAGE_SIZE);
+        dto.setNumberOfItems(users.getTotalElements());
+        dto.setNumberOfPages((long) users.getTotalPages());
+        dto.setUsers(users.get().map(userMapper::toDto).collect(Collectors.toList()));
 
-        LOGGER.info("Received all users service: {}; page: {}; users: {}", serviceKey, page, resultPage);
-        return resultPage;
+        LOGGER.info("Received all users service: {}; page: {}; users: {}", serviceKey, page, dto.getNumberOfItems());
+
+        return dto;
     }
 
     /**
@@ -102,7 +111,7 @@ public class UserService {
      */
     public UserDto getUserById(Long userId) {
         LOGGER.info("Performing task to get all user by user ID {}", userId);
-        Optional<User> user = userDao.findByUserId(userId);
+        Optional<User> user = userDao.findById(userId);
 
         if (user.isEmpty()) {
             LOGGER.warn("User with ID {} cannot be found", userId);
@@ -119,8 +128,7 @@ public class UserService {
      * @param dto User to create.
      * @return Created user object.
      */
-    public UserDto createUserForService(UserCreateDto dto)
-            throws UnsupportedEncodingException, CannotSendEmailException {
+    public UserDto createUserForService(UserCreateDto dto) {
         LOGGER.info("Performing task to create new user with dto: {}", dto);
         Optional<User> optionalUser = userDao.findByUsername(dto.getUsername());
         Optional<Service> service = serviceDao.findByKey(dto.getServiceKey());
@@ -148,13 +156,18 @@ public class UserService {
 
         user.getServiceRoles().add(role);
 
-        // Send email
-        LOGGER.info("Sending welcome email to new user email {}", user.getEmail());
-        mailingService.sendRegistrationEmail(user);
-
         User createdUser = userDao.save(user);
-        LOGGER.info("User created successfully, new user: {}", createdUser);
-        return userMapper.toDto(user);
+        LOGGER.info("User created successfully, new username: {}, id: {}",
+                createdUser.getUsername(), createdUser.getId());
+
+        try {
+            LOGGER.info("Sending welcome email to new user email {}", createdUser.getEmail());
+            mailingService.sendRegistrationEmail(createdUser);
+        } catch (Exception e) {
+            LOGGER.error("Could not send an email for user id: {}, reason: {}", user.getId(), e.getMessage());
+        }
+
+        return userMapper.toDto(createdUser);
     }
 
     /**
@@ -165,7 +178,7 @@ public class UserService {
      */
     public UserDto updateUser(Long id, UserUpdateDto dto) {
         LOGGER.info("Performing task to update user with id: {}", id);
-        Optional<User> optionalUser = userDao.findByUserId(id);
+        Optional<User> optionalUser = userDao.findById(id);
 
         if (optionalUser.isEmpty()) {
             LOGGER.warn("User with id {} from dto has not been found", id);
@@ -196,13 +209,17 @@ public class UserService {
      * Remove user if user can be found, and if user is not uservist.
      * If user has no connection to the serviceKey provided then throw error.
      * @param id Users ID.
-     * @return True if user deleted successfully.
      */
-    public Boolean deleteUser(Long id) {
+    public void deleteUser(Long id) {
         LOGGER.info("Performing task to delete user with ID {}", id);
-        userDao.deleteById(id);
+        Optional<User> optionalUser = userDao.findById(id);
+
+        if (optionalUser.isEmpty()) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
+
+        userDao.delete(optionalUser.get());
 
         LOGGER.info("User deleted successfully");
-        return true;
     }
 }
