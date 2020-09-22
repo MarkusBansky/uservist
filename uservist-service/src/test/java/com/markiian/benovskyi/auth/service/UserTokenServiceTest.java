a5c3f1f3e28a81eace9fa326c75ec5ee299a57bf
Markiian benovskyi <admin@markiian-benovskyi.com>
2020-09-22 15:13:58 +0300

    Fixed the circle ci config

---
+++ uservist-service/src/test/java/com/markiian/benovskyi/auth/service/UserTokenServiceTest.java
@@ -0,0 +1,13 @@
+package com.markiian.benovskyi.auth.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTokenServiceTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
